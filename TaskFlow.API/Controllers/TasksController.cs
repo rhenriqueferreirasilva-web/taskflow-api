e7c52818a6cf003660622defc626e207de657bf6
rhenriqueferreirasilva-web <r.henriqueferreirasilva@gmail.com>
2026-04-24 19:24:39 -0300

    Add TasksController with basic logic

--- TaskFlow.API/Controllers/TasksController.cs
+++ TaskFlow.API/Controllers/TasksController.cs
@@ -1,1 +1,28 @@
+using TaskFlow.API.Models;
+using TaskFlow.API.DTOs;
 
+namespace TaskFlow.API.Controllers;
+
+public class TasksController
+{
+    private static List<TaskItem> tasks = new();
+
+    public List<TaskItem> GetAll()
+    {
+        return tasks;
+    }
+
+    public TaskItem Create(CreateTaskRequest request)
+    {
+        var task = new TaskItem
+        {
+            Id = tasks.Count + 1,
+            Title = request.Title,
+            Done = false,
+            CreatedAt = DateTime.Now
+        };
+
+        tasks.Add(task);
+        return task;
+    }
+}
