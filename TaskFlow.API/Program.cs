4db4a262ccc83649d1ca079c48ab5d2b3e3b7ca6
rhenriqueferreirasilva-web <r.henriqueferreirasilva@gmail.com>
2026-04-24 19:06:32 -0300

    Add initial API structure

--- TaskFlow.API/Program.cs
+++ TaskFlow.API/Program.cs
@@ -1,1 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/tasks", () =>
+{
+    return new[]
+    {
+        new { Id = 1, Title = "Estudar C#", Done = false },
+        new { Id = 2, Title = "Criar API", Done = true }
+    };
+});
+
+app.Run();
